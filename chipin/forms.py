bc26be55c3506795c6e26312dc28a503210ec63e
Harry #2 <harry@gmail.com>
2025-03-19 13:35:27 +1100

    Event Creation

--- chipin/forms.py
+++ chipin/forms.py
@@ -1,6 +1,37 @@
 from django import forms
 from .models import Group
 from .models import Comment
+from django import forms
+from django.core.exceptions import ValidationError
+import mimetypes
+
+class FileUploadForm(forms.Form):
+    file = forms.FileField()
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        # Validate file type (only images allowed)
+        mime_type = mimetypes.guess_type(file.name)[0]
+        if not mime_type or not mime_type.startswith('image'):
+            raise ValidationError("Only image files are allowed.")
+        # Validate file size (limit to 2MB)
+        if file.size > 2 * 1024 * 1024:
+            raise ValidationError("File size exceeds 2MB limit.")
+        
+        return file
+
+class UserProfilePictureForm(forms.Form):
+    picture = forms.ImageField()
+    def clean_picture(self):
+        picture = self.cleaned_data.get('picture')
+        # Restrict file types
+        if not picture.content_type.startswith('image'):
+            raise forms.ValidationError("Only image files are allowed.")
+        
+        # Restrict file size (limit to 1MB)
+        if picture.size > 1024 * 1024:
+            raise forms.ValidationError("The image file is too large (limit: 1MB).")
+        
+        return picture
 
 class CommentForm(forms.ModelForm):
     class Meta:
