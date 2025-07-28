66a2c5c3b1e5e8be90e823ea55349c070b9925f5
aaabbbccc11283 <harrisonschool666@gmail.com>
2025-07-28 12:02:46 +1000

    fixed groups html files with right names

--- chipin/admin.py
+++ chipin/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
-
-# Register your models here.
+                <!-- Allow the comment owner or admin to edit or delete -->
+		{% if comment.user == request.user or request.user == group.admin %}
+                    <a href="{% url 'chipin:edit_comment' group.id comment.id %}">Edit</a>
+                    <a href="{% url 'chipin:delete_comment' comment.id %}" onclick="return confirm('Are you sure?')">Delete</a>
+                {% endif %}
