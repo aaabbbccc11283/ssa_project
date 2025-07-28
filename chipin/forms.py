483762e7c56c8e706a8c7cde1d5b501b99848e34
aaabbbccc11283 <harrisonschool666@gmail.com>
2025-07-28 12:17:34 +1000

    Okay all HTML SHOULD be fixed now

--- chipin/forms.py
+++ chipin/forms.py
@@ -10,7 +10,7 @@ class GroupCreationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
- 
+
     def save(self, commit=True):
         group = super().save(commit=False)
         group.admin = self.user  # Assign the logged-in user as the admin
